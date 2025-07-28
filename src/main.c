4d3840379183f09a90bb68ebe9776cb96b036f6d
itsmeodx <oussama15ouaadich@gmail.com>
2025-07-28 16:56:44 +0100

    Improves server robustness and logging
    Adds error handling for send failures and enhances logging to indicate server status and response transmission. Clarifies socket closure steps to prevent resource leaks, increasing code reliability and maintainability.

--- src/main.c
+++ src/main.c
@@ -9,30 +9,34 @@
 #include <unistd.h>
 
 #define PORT "4221" // Port number for the server
-#define BACKLOG 10 // Number of pending connections queue
+#define BACKLOG 10	// Number of pending connections queue
 
-int main() {
+int main()
+{
 	// Disable output buffering
 	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+	setbuf(stderr, NULL);
 
+	// Initialize address information
 	struct addrinfo hints, *servAddr;
 
 	hints = (struct addrinfo){0};
-	hints.ai_family = AF_INET; // IPv4
+	hints.ai_family = AF_INET;		 // IPv4
 	hints.ai_socktype = SOCK_STREAM; // TCP
-	hints.ai_flags = AI_PASSIVE; // For wildcard IP address
+	hints.ai_flags = AI_PASSIVE;	 // For wildcard IP address
 
 	// Get address information for the server
 	int status = getaddrinfo(NULL, PORT, &hints, &servAddr);
-	if (status != 0) {
+	if (status != 0)
+	{
 		fprintf(stderr, "server: getaddrinfo: %s\n", gai_strerror(status));
 		return (EXIT_FAILURE);
 	}
 
 	// Create a socket
 	int sockfd = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
-	if (sockfd == -1) {
+	if (sockfd == -1)
+	{
 		perror("server: socket");
 		freeaddrinfo(servAddr);
 		return (EXIT_FAILURE);
@@ -40,7 +44,8 @@ int main() {
 
 	// Set socket options to allow reuse of the address
 	int optval = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+	{
 		perror("server: setsockopt");
 		close(sockfd);
 		freeaddrinfo(servAddr);
@@ -48,7 +53,8 @@ int main() {
 	}
 
 	// Bind the socket to the address
-	if (bind(sockfd, servAddr->ai_addr, servAddr->ai_addrlen) == -1) {
+	if (bind(sockfd, servAddr->ai_addr, servAddr->ai_addrlen) == -1)
+	{
 		perror("server: bind");
 		close(sockfd);
 		freeaddrinfo(servAddr);
@@ -58,20 +64,24 @@ int main() {
 	// Free the address information structure
 	freeaddrinfo(servAddr);
 
+	printf("server: running on port %s\n", PORT);
+
 	// Listen for incoming connections
-	if (listen(sockfd, BACKLOG) == -1) {
+	if (listen(sockfd, BACKLOG) == -1)
+	{
 		perror("server: listen");
 		close(sockfd);
 		return (EXIT_FAILURE);
 	}
 
-	printf("server: waiting for connections on port %s...\n", PORT);
+	printf("server: waiting for connections...\n", PORT);
 
 	// Accept incoming connections
 	struct sockaddr_storage clientAddr;
 	socklen_t clientAddrLen = sizeof(clientAddr);
 	int clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
-	if (clientSockfd == -1) {
+	if (clientSockfd == -1)
+	{
 		perror("server: accept");
 		close(sockfd);
 		return (EXIT_FAILURE);
@@ -81,13 +91,24 @@ int main() {
 
 	// Send HTTP response
 	const char *httpResponse = "HTTP/1.1 200 OK\r\n"
-		"Content-Type: text/plain\r\n"
-		"Content-Length: 14\r\n"
-		"\r\n"
-		"Hello, world!\n";
-	send(clientSockfd, httpResponse, strlen(httpResponse), 0);
+							   "Content-Type: text/plain\r\n"
+							   "Content-Length: 14\r\n"
+							   "\r\n"
+							   "Hello, world!\n";
+	if (send(clientSockfd, httpResponse, strlen(httpResponse), 0) == -1)
+	{
+		perror("server: send");
+		close(clientSockfd);
+		close(sockfd);
+		return (EXIT_FAILURE);
+	}
 
+	printf("server: response sent\n");
+
+	// Close the client socket
 	close(clientSockfd);
+
+	// Close the server socket
 	close(sockfd);
 	return 0;
 }
