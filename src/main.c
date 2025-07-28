759f247df17400836a1e718fcde22d88216e10eb
itsmeodx <oussama15ouaadich@gmail.com>
2025-07-28 16:37:10 +0100

    Refactors server to use getaddrinfo and send HTTP response
    Replaces manual socket setup with getaddrinfo for flexibility and future IPv6 support. Adds logic to accept a client connection and respond with a basic HTTP 200 OK message, ensuring the server correctly communicates as an HTTP server. Improves robustness and clarity of the server initialization process.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -7,56 +8,86 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define PORT "4221" // Port number for the server
+#define BACKLOG 10 // Number of pending connections queue
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+	struct addrinfo hints, *servAddr;
 
-	// Uncomment this block to pass the first stage
+	hints = (struct addrinfo){0};
+	hints.ai_family = AF_INET; // IPv4
+	hints.ai_socktype = SOCK_STREAM; // TCP
+	hints.ai_flags = AI_PASSIVE; // For wildcard IP address
 
-	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
+	// Get address information for the server
+	int status = getaddrinfo(NULL, PORT, &hints, &servAddr);
+	if (status != 0) {
+		fprintf(stderr, "server: getaddrinfo: %s\n", gai_strerror(status));
+		return (EXIT_FAILURE);
 	}
 
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
+	// Create a socket
+	int sockfd = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
+	if (sockfd == -1) {
+		perror("server: socket");
+		freeaddrinfo(servAddr);
+		return (EXIT_FAILURE);
 	}
 
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
+	// Set socket options to allow reuse of the address
+	int optval = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+		perror("server: setsockopt");
+		close(sockfd);
+		freeaddrinfo(servAddr);
+		return (EXIT_FAILURE);
 	}
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
+	// Bind the socket to the address
+	if (bind(sockfd, servAddr->ai_addr, servAddr->ai_addrlen) == -1) {
+		perror("server: bind");
+		close(sockfd);
+		freeaddrinfo(servAddr);
+		return (EXIT_FAILURE);
 	}
 
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
+	// Free the address information structure
+	freeaddrinfo(servAddr);
 
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
+	// Listen for incoming connections
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("server: listen");
+		close(sockfd);
+		return (EXIT_FAILURE);
+	}
 
-	close(server_fd);
+	printf("server: waiting for connections on port %s...\n", PORT);
 
+	// Accept incoming connections
+	struct sockaddr_storage clientAddr;
+	socklen_t clientAddrLen = sizeof(clientAddr);
+	int clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
+	if (clientSockfd == -1) {
+		perror("server: accept");
+		close(sockfd);
+		return (EXIT_FAILURE);
+	}
+
+	printf("server: connection accepted\n");
+
+	// Send HTTP response
+	const char *httpResponse = "HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/plain\r\n"
+		"Content-Length: 14\r\n"
+		"\r\n"
+		"Hello, world!\n";
+	send(clientSockfd, httpResponse, strlen(httpResponse), 0);
+
+	close(clientSockfd);
+	close(sockfd);
 	return 0;
 }
